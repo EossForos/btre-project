a6c9ec7bbce2791b7d104e90a3674d48dd200d9e
EossForos <volosonopulos@gmail.com>
2020-03-21 14:28:21 +0200

    pagination & filter listing

--- btre/settings.py
+++ btre/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'pages',
     'listings',
@@ -129,6 +130,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 try:
     from .local_settings import *
--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,18 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'listings/listings.html')
+from .models import Listing
 
-def listing(request):
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    return render(request, 'listings/listings.html', {'listings':paged_listings})
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
